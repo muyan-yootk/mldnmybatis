d2f0b5cec75d3f35911a189b2f05bcb5c333cc1b
mldn <mldnqa@163.com>
2018-06-05 17:24:47 +0800

    实现数据更新操作。

--- mldnmybatis-base/src/test/java/cn/mldn/mldnmybatis/base/TestNews.java
+++ mldnmybatis-base/src/test/java/cn/mldn/mldnmybatis/base/TestNews.java
@@ -9,13 +9,32 @@ import cn.mldn.vo.News;
 
 public class TestNews {
 	@Test
+	public void testEdit() throws Exception {
+		News vo = new News() ;
+		vo.setTitle("马上下课了，高兴吗？");
+		vo.setPubdate(new Date());
+		vo.setNid(6); 
+		System.out.println(MyBatisSessionFactory.getSession().update(
+				"cn.mldn.mapping.NewsNS.doEdit", vo)); 
+		MyBatisSessionFactory.getSession().commit(); // 提交事务 
+		MyBatisSessionFactory.close();
+	}
+	@Test
+	public void testRemove() throws Exception {
+		System.out.println(MyBatisSessionFactory.getSession().delete(
+				"cn.mldn.mapping.NewsNS.doRemove", 9L)); 
+		MyBatisSessionFactory.getSession().commit(); // 提交事务 
+		MyBatisSessionFactory.close();
+	}
+	@Test
 	public void testAdd() throws Exception {
 		// 1、创建VO对象，并且为对象设置内容
 		News vo = new News() ;
 		vo.setTitle("今天是个好天气。");
 		vo.setPubdate(new Date());
 		// 2、通过SqlSession进行内容的设置；
-		System.out.println(MyBatisSessionFactory.getSession().insert("cn.mldn.mapping.NewsNS.doCreate", vo));
+		System.out.println(MyBatisSessionFactory.getSession().delete(
+				"cn.mldn.mapping.NewsNS.doCreate", vo)); 
 		// 3、在MyBatis里面默认都是开启事务的，所以如果没有进行事务的提交将无法正常进行处理
 		MyBatisSessionFactory.getSession().commit(); // 提交事务
 		System.out.println("当前的ID内容：" + vo.getNid());
