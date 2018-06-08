8b897634b653ad739ea5f0e94454769b91fd769c
mldn <mldnqa@163.com>
2018-06-08 14:38:35 +0800

    缓存统一配置。

--- mldnmybatis-ssm/src/main/java/cn/mldn/ssm/service/IEmpService.java
+++ mldnmybatis-ssm/src/main/java/cn/mldn/ssm/service/IEmpService.java
@@ -3,24 +3,26 @@ package cn.mldn.ssm.service;
 import java.util.Map;
 import java.util.Set;
 
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
 import cn.mldn.ssm.vo.Emp;
 
+@CacheConfig(cacheNames = "emp")
 public interface IEmpService {
 	public boolean add(Emp vo) ;
 
-	@CacheEvict(cacheNames = "emp", key="#ids[0]")
+	@CacheEvict(key="#ids[0]")
 	public boolean delete(Set<Long> ids) ; 
 	
-	@CachePut(cacheNames = "emp", key = "#vo.empno", unless = "#result == null")
+	@CachePut(key = "#vo.empno", unless = "#result == null")
 	public Emp edit2(Emp vo);
 	
 	public boolean edit(Emp vo);
 
-	@Cacheable(cacheNames = "emp", key = "#eid", sync = true , condition="#eid<8000" , unless="#result == null")
+	@Cacheable(key = "#eid", sync = true , condition="#eid<8000" , unless="#result == null")
 	public Emp getEmp(long eid, String ena);
 	
 	@Cacheable(cacheNames = "emp")
--- mldnmybatis-ssm/src/test/java/cn/mldn/ssm/test/TestEmpCache.java
+++ mldnmybatis-ssm/src/test/java/cn/mldn/ssm/test/TestEmpCache.java
@@ -55,13 +55,13 @@ public class TestEmpCache {
 	
 	@Test
 	public void testGet() {
-		Emp voA = this.empService.getEmp(7369L,"小强") ;	// id作为缓存
+		Emp voA = this.empService.preEdit(7397L) ;	// id作为缓存
 		System.out.println(voA);
 		System.out.println("********************************");
-		Emp voB = this.empService.getEmp(7369L,"小强") ;	// 根据id查询
+		Emp voB = this.empService.preEdit(7397L) ;	// 根据id查询
 		System.out.println(voB);
 		System.out.println("********************************");
-		Emp voC = this.empService.preEdit(7369L) ;	// 根据id查询
+		Emp voC = this.empService.preEdit(7397L) ;	// 根据id查询
 		System.out.println(voC);
 	}
 }
